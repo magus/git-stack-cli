c6fa422f459b9957375f521008be3dd0ec9c4123
magus <noah@iamnoah.com>
2023-10-29 01:54:20 -0700

    core/match_group for regex group

---
+++ src/core/match_group.ts
@@ -0,0 +1,10 @@
+import { invariant } from "./invariant.js";
+
+export function match_group(value: string, re: RegExp, group: string) {
+  const match = value.match(re);
+  const debug = `[${value}.match(${re})]`;
+  invariant(match?.groups, `match.groups must exist ${debug}`);
+  const result = match?.groups[group];
+  invariant(result, `match.groups must contain [${group}] ${debug}`);
+  return result;
+}
