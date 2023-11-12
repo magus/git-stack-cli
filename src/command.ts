0dadf98299fdfa15a156887966628ac40589f7a7
magus <noah@iamnoah.com>
2023-11-12 13:09:41 -0800

    command: descriptions and strict

--- src/command.ts
+++ src/command.ts
@@ -4,25 +4,33 @@ import { hideBin } from "yargs/helpers";
 export type Argv = Awaited<ReturnType<typeof command>>;
 
 export async function command() {
-  return yargs(hideBin(process.argv))
-    .option("force", {
-      type: "boolean",
-      description: "force",
-    })
+  return (
+    yargs(hideBin(process.argv))
+      .option("force", {
+        type: "boolean",
+        description: "Force sync even if no changes are detected",
+      })
 
-    .option("check", {
-      type: "boolean",
-      description: "check",
-    })
+      .option("check", {
+        type: "boolean",
+        description: "Print status table without syncing",
+      })
 
-    .option("debug", {
-      type: "boolean",
-      description: "debug",
-    })
+      .option("verbose", {
+        type: "boolean",
+        description: "Log extra information during execution",
+      })
 
-    .option("mock-metadata", {
-      type: "boolean",
-      description: "mock-metadata",
-    })
-    .help().argv;
+      .option("debug", {
+        type: "boolean",
+        description: "Log debugging information during execution",
+      })
+      .option("mock-metadata", {
+        type: "boolean",
+        description: "Mock local store metadata for testing",
+      })
+      // disallow unknown options
+      .strict()
+      .help().argv
+  );
 }
