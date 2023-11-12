357d3df63e244b07a18185a168d38d4555890fe9
magus <noah@iamnoah.com>
2023-11-12 15:44:20 -0800

    command: debug mode-only options

--- src/command.ts
+++ src/command.ts
@@ -4,6 +4,38 @@ import { hideBin } from "yargs/helpers";
 export type Argv = Awaited<ReturnType<typeof command>>;
 
 export async function command() {
+  const debug_argv = await yargs(hideBin(process.argv))
+    .option("debug", {
+      type: "boolean",
+      description: "Enable debug mode with more options for debugging",
+    })
+
+    .help(false).argv;
+
+  if (!debug_argv.debug) {
+    return (
+      yargs(hideBin(process.argv))
+        .option("force", {
+          type: "boolean",
+          description: "Force sync even if no changes are detected",
+        })
+
+        .option("check", {
+          type: "boolean",
+          description: "Print status table without syncing",
+        })
+
+        .option("debug", {
+          type: "boolean",
+          description: "Enable debug mode with more options for debugging",
+        })
+
+        // disallow unknown options
+        .strict()
+        .help().argv
+    );
+  }
+
   return (
     yargs(hideBin(process.argv))
       .option("force", {
@@ -16,14 +48,14 @@ export async function command() {
         description: "Print status table without syncing",
       })
 
+      .option("debug", {
+        type: "boolean",
+        description: "Enable debug mode with more options for debugging",
+      })
+
       .option("verbose", {
         type: "boolean",
         description: "Log extra information during execution",
-      })
-
-      .option("debug", {
-        type: "boolean",
-        description: "Log debugging information during execution",
       })
 
       .option("write-state-json", {
