69e3b8b061243835bbd2b394021dcf765df38407
magus <noah@iamnoah.com>
2023-11-17 02:52:34 -0800

    TODO

---
+++ TODO.md
@@ -0,0 +1,58 @@
+# TODO
+
+- auto update
+  - check npm for latest version
+  - compare against local package.json
+  - offer to update YesNoPrompt
+
+- when MERGED pr group detected, YesNoPrompt to fetch and rebase (equivalent to git sync)
+
+- `id.js` create shorter unique identifiers instead of uuid (too long!)
+
+- `--debug` show output from git push command
+
+- add `-—no-verify` flag to pass into git push to skip pre-commit (sync step is slow on work laptop because of this I suspect)
+
+- capture PR title when creating new group
+
+- ManualRebase
+  - set nice PR title and summary (diff stack table)
+
+  - build table of stacked PRs to add to each PR as comment
+    - order based on local sha ordering (i.e. group_list, which reflects PR order)
+    - delete and update comment in pr if necessary (use regex)
+
+
+- interactive PR status table
+  - ▶ Unassigned (8 commits)
+  - ▶ #764 Title B (2/3 commits)  https://github.com/...
+  - ▶ #742 Title A (5/5 commits)  https://github.com/...
+
+  - `folder: { on: '▼', off: '▶' },`
+  - the first local row shows new commits not synced to any PR
+  - (2/3 commits) means 3 commits locally and 2 commits in remote (mismatch)
+  - focus + enter toggles the list open to view each commit
+  - when expanded
+    - each commit should also show status, e.g. new commits should show with NEW
+    - show actions, e.g. Shift+D -> Delete
+
+
+- multiselect with more items than terminal rows needs pagination
+  - maybe default to window size of about 10 and display a scrollbar to indicate more items in either direction?
+  - allow typing to filder the list based on regex match?
+  - quicksearch may be similar or at least inspo
+  - https://github.com/Eximchain/ink-quicksearch-input
+
+## names
+
+- stack
+- prs
+- batch
+- cake
+- pancake
+- bake
+- pile
+- ladder
+- steps
+- stairs
+- chain
---
+++ src/core/id.ts
@@ -0,0 +1,89 @@
+import crypto from "node:crypto";
+
+// console.log(id());
+
+export function id() {
+  const timestamp = Date.now();
+
+  // 9 223 372 036 854 775 808
+  // 9 trillion possible values
+  // (2^53) * (2^10) = 2^63 = 9,223,372,036,854,775,808
+  const js_max_bits = 53;
+
+  const timestamp_bits = Math.floor(Math.log2(timestamp)) + 1;
+
+  // padding needed to reach 53 bits
+  const padding_bits = js_max_bits - timestamp_bits;
+
+  // random between 0 and 2^padding_bits - 1
+  const random = crypto.randomInt(0, Math.pow(2, padding_bits));
+
+  // combine timestamp and random value
+  const combined = interleave_bits(timestamp, random);
+
+  // console.debug({ combined, timestamp, random, padding_bits, timestamp_bits });
+
+  return encode(combined);
+}
+
+function binary(value: number) {
+  return BigInt(value).toString(2);
+}
+
+function rand_index(list: Array<any>) {
+  return Math.floor(Math.random() * list.length);
+}
+
+function interleave_bits(a: number, b: number) {
+  const a_binary = binary(a).split("");
+
+  const b_binary = binary(b).split("");
+
+  while (b_binary.length) {
+    // pull random bit out of b_binary
+
+    const b_index = rand_index(b_binary);
+
+    const [selected] = b_binary.splice(b_index, 1);
+
+    // insert random bit into a_binary
+
+    const a_index = rand_index(a_binary);
+
+    a_binary.splice(a_index, 0, selected);
+  }
+
+  // convert binary list back to integer
+
+  const a_value = parseInt(a_binary.join(""), 2);
+
+  return a_value;
+}
+
+function encode(value: number) {
+  // base64 encode (64 characters)
+  // max character necessary to encode is equal to maximum number
+  // of bits in value divided by bits per character in encoding
+  //
+  // Example
+  //   in base64 each characters can represent 6 bits (2^6 = 64)
+  //   53 bits / 6 bits = 8.833333333333334 characters (9 characters)
+  //
+  // prettier-ignore
+  const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-+";
+
+  const bits_per_char = Math.log2(chars.length);
+  const max_value_bits = 53;
+  const max_char_size = Math.ceil(max_value_bits / bits_per_char);
+
+  let result = "";
+
+  while (value > 0) {
+    result = chars[value % chars.length] + result;
+
+    value = Math.floor(value / chars.length);
+  }
+
+  // pad the result to necessary characters
+  return result.padStart(max_char_size, "=");
+}
