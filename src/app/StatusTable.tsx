3cff485a8f1caa1cf83aa31cb3735ac5a5fa0c08
magus <noah@iamnoah.com>
2023-11-24 13:11:51 -0800

    StatusTable: generic Table

--- src/app/StatusTable.tsx
+++ src/app/StatusTable.tsx
@@ -2,9 +2,12 @@ import * as React from "react";
 
 import * as Ink from "ink";
 
+import { assertNever } from "../core/assertNever.js";
 import { invariant } from "../core/invariant.js";
+import { is_finite_value } from "../core/is_finite_value.js";
 
 import { Store } from "./Store.js";
+import { Url } from "./Url.js";
 
 export function StatusTable() {
   const commit_range = Store.useState((state) => state.commit_range);
@@ -15,10 +18,9 @@ export function StatusTable() {
 
   for (const group of commit_range.group_list) {
     const row: Row = {
-      id: group.id,
       icon: "",
       count: "",
-      status: "",
+      status: "NEW",
       title: "",
       url: "",
     };
@@ -56,7 +58,35 @@ export function StatusTable() {
     row_list.push(row);
   }
 
-  if (!row_list.length) {
+  return (
+    <Table
+      data={row_list}
+      fillColumn="title"
+      // maxWidth={{
+      //   title: 30,
+      // }}
+      columnGap={3}
+      columns={{
+        icon: IconColumn,
+        status: StatusColumn,
+        count: CountColumn,
+        title: TitleColumn,
+        url: UrlColumn,
+      }}
+    />
+  );
+}
+
+type Props<T extends BaseRow> = {
+  data: Array<T>;
+  columns: ColumnComponentMap<T>;
+  columnGap?: number;
+  fillColumn?: Column<T>;
+  maxWidth?: Partial<{ [key in Column<T>]: number }>;
+};
+
+function Table<T extends BaseRow>(props: Props<T>) {
+  if (!props.data.length) {
     return (
       <Container>
         <Ink.Text dimColor>No data found.</Ink.Text>
@@ -64,64 +94,79 @@ export function StatusTable() {
     );
   }
 
+  const sample_row = props.data[0];
+  const RowColumnList = Object.keys(sample_row) as Array<Column<T>>;
+
   // walk data and discover max width for each column
-  const max_col_width = {} as {
-    [key in (typeof RowColumnList)[number]]: number;
-  };
+  const max_col_width = {} as { [key in Column<T>]: number };
 
   for (const col of RowColumnList) {
     max_col_width[col] = 0;
   }
 
-  for (const row of row_list) {
+  for (const row of props.data) {
     for (const col of RowColumnList) {
       const row_col = row[col];
-      max_col_width[col] = Math.max(row_col.length, max_col_width[col]);
+      max_col_width[col] = Math.max(String(row_col).length, max_col_width[col]);
+      const maxWidth = props.maxWidth?.[col];
+      if (is_finite_value(maxWidth)) {
+        max_col_width[col] = Math.min(maxWidth - 1, max_col_width[col]);
+      }
     }
   }
 
   const { stdout } = Ink.useStdout();
   const available_width = stdout.columns;
-  const columnGap = 2;
+  const columnGap = is_finite_value(props.columnGap) ? props.columnGap : 2;
   const breathing_room = 0;
 
-  const remaining_space =
-    available_width -
-    // icon
-    max_col_width.icon -
-    // status
-    max_col_width.status -
-    // commits
-    max_col_width.count -
-    // url
-    max_col_width.url -
-    // gap * col count
-    columnGap * RowColumnList.length -
-    // remove some extra space
-    breathing_room;
+  if (props.fillColumn) {
+    let remaining_space = available_width;
 
-  // add one for ellipsis character
-  max_col_width.title = Math.min(max_col_width.title, remaining_space + 1);
+    for (const col of RowColumnList) {
+      // skip fill column from calculation
+      if (props.fillColumn === col) {
+        continue;
+      }
+
+      remaining_space -= max_col_width[col];
+    }
+
+    // gap * col count
+    remaining_space -= columnGap * (RowColumnList.length - 1);
+
+    // remove some extra space
+    remaining_space -= breathing_room;
+
+    if (props.fillColumn) {
+      max_col_width[props.fillColumn] = Math.min(
+        max_col_width[props.fillColumn],
+        remaining_space
+      );
+    }
+  }
 
   // console.debug({ available_width, remaining_space, max_col_width });
 
   return (
     <Container>
-      {row_list.map((row) => {
+      {props.data.map((row, i) => {
         return (
           <Ink.Box
-            key={row.id}
+            key={i}
             // borderStyle="round"
             flexDirection="row"
             columnGap={columnGap}
             width={available_width}
           >
             {RowColumnList.map((column) => {
-              const ColumnComponent = ColumnComponentMap[column];
+              const ColumnComponent = props.columns[
+                column
+              ] as ColumnComponent<T>;
 
               return (
-                <Ink.Box key={column} width={max_col_width[column]}>
-                  <ColumnComponent key={column} row={row} column={column} />
+                <Ink.Box key={String(column)} width={max_col_width[column]}>
+                  <ColumnComponent row={row} column={column} />
                 </Ink.Box>
               );
             })}
@@ -142,55 +187,98 @@ function Container(props: { children: React.ReactNode }) {
   );
 }
 
-const RowColumnList = ["icon", "status", "count", "title", "url"] as const;
-
-type Column = (typeof RowColumnList)[number];
-type Row = { [key in Column]: string } & { id: string };
-
-const ColumnComponentMap: ColumnComponentMapType = {
-  icon: Icon,
-  status: Status,
-  count: Count,
-  title: Title,
-  url: Url,
+type Row = {
+  icon: string;
+  status: "NEW" | "OUTDATED" | "MERGED" | "SYNCED";
+  count: string;
+  title: string;
+  url: string;
 };
 
-type ColumnComponentMapType = {
-  [key in Column]: (props: ColumnProps) => React.ReactNode;
+type BaseRow = Record<string, string | number>;
+
+type Column<T extends BaseRow> = keyof T;
+
+type ColumnComponent<T extends BaseRow> = (
+  props: ColumnProps<T>
+) => React.ReactNode;
+
+type ColumnComponentMap<T extends BaseRow> = Record<
+  Column<T>,
+  ColumnComponent<T>
+>;
+
+type ColumnProps<T extends BaseRow> = {
+  row: T;
+  column: keyof T;
 };
 
-type ColumnProps = { row: Row; column: Column };
-
-function Icon(props: ColumnProps) {
+function IconColumn(props: ColumnProps<Row>) {
   const value = props.row[props.column];
 
   return (
-    <Ink.Text color="yellow" bold>
+    <Ink.Text
+      color={get_status_color(props.row)}
+      bold={get_status_bold(props.row)}
+    >
       {value}
     </Ink.Text>
   );
 }
 
-function Status(props: ColumnProps) {
+function StatusColumn(props: ColumnProps<Row>) {
   const value = props.row[props.column];
 
-  return <Ink.Text>{value}</Ink.Text>;
+  return (
+    <Ink.Text
+      color={get_status_color(props.row)}
+      bold={get_status_bold(props.row)}
+    >
+      {value}
+    </Ink.Text>
+  );
 }
 
-function Count(props: ColumnProps) {
+function CountColumn(props: ColumnProps<Row>) {
   const value = props.row[props.column];
 
-  return <Ink.Text>{value}</Ink.Text>;
+  return <Ink.Text dimColor>{value}</Ink.Text>;
 }
 
-function Title(props: ColumnProps) {
+function TitleColumn(props: ColumnProps<Row>) {
   const value = props.row[props.column];
 
   return <Ink.Text wrap="truncate-end">{value}</Ink.Text>;
 }
 
-function Url(props: ColumnProps) {
+function UrlColumn(props: ColumnProps<Row>) {
   const value = props.row[props.column];
 
-  return <Ink.Text>{value}</Ink.Text>;
+  return <Url dimColor>{value}</Url>;
+}
+
+function get_status_color(row: Row) {
+  switch (row.status) {
+    case "NEW":
+      return "yellow";
+    case "OUTDATED":
+      return "red";
+    case "MERGED":
+      return "purple";
+    case "SYNCED":
+      return "green";
+    default:
+      assertNever(row.status);
+      return "gray";
+  }
+}
+
+function get_status_bold(row: Row) {
+  switch (row.status) {
+    case "NEW":
+    case "OUTDATED":
+      return true;
+    default:
+      return false;
+  }
 }
--- src/app/Url.tsx
+++ src/app/Url.tsx
@@ -2,15 +2,17 @@ import * as React from "react";
 
 import * as Ink from "ink";
 
-type Props = {
+type Props = InkTextProps & {
   children: React.ReactNode;
 };
+
+type InkTextProps = React.ComponentProps<typeof Ink.Text>;
 
 export function Url(props: Props) {
   const text_color = "#38bdf8";
 
   return (
-    <Ink.Text bold color={text_color}>
+    <Ink.Text bold color={text_color} {...props}>
       {props.children}
     </Ink.Text>
   );
