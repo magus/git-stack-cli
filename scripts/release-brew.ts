e9c0e27bf8bbccb05931428a78a2a087512ea7dc
magus <noah@iamnoah.com>
2024-02-11 20:24:04 -0800

    scripts/release-brew: use new Formula + templates

--- scripts/release-brew.ts
+++ scripts/release-brew.ts
@@ -29,7 +29,9 @@ const re_token = (name: string) => new RegExp(`{{ ${name} }}`, "g");
 
 process.chdir(HOMEBREW_DIR);
 
-let formula = await file.read_text("git-stack.rb.template");
+let formula = await file.read_text(
+  path.join("templates", "git-stack.tap.rb.template")
+);
 
 formula = formula.replace(re_token("version"), version);
 formula = formula.replace(re_token("mac_bin"), macos_asset.filepath);
@@ -37,27 +39,19 @@ formula = formula.replace(re_token("mac_sha256"), macos_asset.sha256);
 formula = formula.replace(re_token("linux_bin"), linux_asset.filepath);
 formula = formula.replace(re_token("linux_sha256"), linux_asset.sha256);
 
-await file.write_text("git-stack.rb", formula);
+await file.write_text(path.join("Formula", "git-stack.rb"), formula);
 
+// commit homebrew repo changes
 await spawn.sync(`git commit -a -m ${version}`);
 await spawn.sync(`git push`);
 
+// commmit changes to main repo
 process.chdir(PROJECT_DIR);
-
-await spawn(`npm i`);
-
-await spawn.sync(`git commit -a -m ${version}`);
+await spawn.sync(`git commit -a -m "homebrew-git-stack ${version}"`);
 await spawn.sync(`git push`);
 
-// -a: create tag on last commit
-// -m: message
-await spawn.sync(`git tag -a ${version} -m ${version}`);
-await spawn.sync(`git push origin ${version}`);
-
-await spawn.sync(`gh release create ${version} -t ${version} --generate-notes`);
-
+// finally upload the assets to the github release
 process.chdir(STANDALONE_DIR);
-
 await spawn.sync(`gh release upload ${version} ${linux_asset.filepath}`);
 await spawn.sync(`gh release upload ${version} ${macos_asset.filepath}`);
 await spawn.sync(`gh release upload ${version} ${win_asset.filepath}`);
@@ -66,4 +60,6 @@ console.debug();
 console.debug("✅", "published", version);
 console.debug();
 console.debug("https://github.com/magus/homebrew-git-stack");
+console.debug();
+console.debug("https://github.com/magus/git-stack-cli/releases");
 console.debug();
