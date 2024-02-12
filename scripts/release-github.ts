45a8385b89341de7243a956e73a0eaebe08dcb55
magus <noah@iamnoah.com>
2024-02-11 18:24:01 -0800

    scripts/release-github

---
+++ scripts/release-github.ts
@@ -0,0 +1,34 @@
+import path from "node:path";
+
+import { create_asset } from "~/core/create_asset";
+import * as file from "~/core/file";
+import { spawn } from "~/core/spawn";
+
+// get paths relative to this script
+const SCRIPT_DIR = import.meta.dir;
+const PROJECT_DIR = path.join(SCRIPT_DIR, "..");
+
+const package_json = await file.read_json(
+  path.join(PROJECT_DIR, "package.json")
+);
+
+const version = package_json.version;
+
+await spawn("npm pack");
+
+// prettier-ignore
+const tarball_asset = (
+  await create_asset(`git-stack-cli-${version}.tgz`, { version })
+);
+
+await spawn.sync(`gh release create ${version} -t ${version} --generate-notes`);
+
+await spawn.sync(`gh release upload ${version} ${tarball_asset.filepath}`);
+
+await file.rm(tarball_asset.filepath);
+
+console.debug();
+console.debug("✅", "published", version);
+console.debug();
+console.debug("https://github.com/magus/git-stack-cli/releases");
+console.debug();
