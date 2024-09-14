47704bb0e9e2d1e561733a18c02a43f9d80e10d9
magus <noah@iamnoah.com>
2024-09-13 19:33:26 -0700

    command: default + fixup commands

--- src/command.ts
+++ src/command.ts
@@ -24,94 +24,117 @@ export async function command() {
   // https://yargs.js.org/docs/#api-reference-optionkey-opt
   return (
     yargs(hideBin(process.argv))
-      .usage("Usage: git stack [options]")
+      .usage("Usage: git stack [command] [options]")
 
-      .option("force", {
-        type: "boolean",
-        alias: ["f"],
-        default: false,
-        description: "Force sync even if no changes are detected",
-      })
+      .command(
+        "$0", // Default command
+        "Sync commit ranges to Github",
+        (yargs) => {
+          return yargs
+            .option("force", {
+              type: "boolean",
+              alias: ["f"],
+              default: false,
+              description: "Force sync even if no changes are detected",
+            })
 
-      .option("check", {
-        type: "boolean",
-        alias: ["c"],
-        default: false,
-        description: "Print status table and exit without syncing",
-      })
+            .option("check", {
+              type: "boolean",
+              alias: ["c"],
+              default: false,
+              description: "Print status table and exit without syncing",
+            })
 
-      .option("sync", {
-        type: "boolean",
-        alias: ["s"],
-        default: true,
-        description: "Sync commit ranges to Github, disable with --no-sync",
-      })
+            .option("sync", {
+              type: "boolean",
+              alias: ["s"],
+              default: true,
+              description:
+                "Sync commit ranges to Github, disable with --no-sync",
+            })
 
-      .option("verify", {
-        type: "boolean",
-        default: true,
-        description:
-          "Run git hooks such as pre-commit and pre-push, disable with --no-verify",
-      })
+            .option("verify", {
+              type: "boolean",
+              default: true,
+              description:
+                "Run git hooks such as pre-commit and pre-push, disable with --no-verify",
+            })
 
-      .option("rebase", {
-        type: "string",
-        choices: [Rebase["git-revise"], Rebase["cherry-pick"]],
-        default: Rebase["git-revise"],
-        description: [
-          "Strategy used for syncing branches",
-          `${Rebase["git-revise"]}: perform faster in-memory rebase`,
-          `${Rebase["cherry-pick"]}: use disk and incrementally rebase each commit`,
-        ].join(" | "),
-      })
+            .option("rebase", {
+              type: "string",
+              choices: [Rebase["git-revise"], Rebase["cherry-pick"]],
+              default: Rebase["git-revise"],
+              description: [
+                "Strategy used for syncing branches",
+                `${Rebase["git-revise"]}: perform faster in-memory rebase`,
+                `${Rebase["cherry-pick"]}: use disk and incrementally rebase each commit`,
+              ].join(" | "),
+            })
+
+            .option("update", {
+              type: "boolean",
+              alias: ["u", "upgrade"],
+              default: false,
+              description: "Check and install the latest version",
+            })
+
+            .option("branch", {
+              type: "string",
+              alias: ["b"],
+              description:
+                'Set the master branch name, defaults to "master" (or "main" if "master" is not found)',
+            })
+
+            .option("draft", {
+              type: "boolean",
+              alias: ["d"],
+              default: false,
+              description: "Open all PRs as drafts",
+            })
+
+            .option("write-state-json", {
+              hidden: true,
+              type: "boolean",
+              default: false,
+              description: "Write state to local json file for debugging",
+            })
+
+            .option("template", {
+              type: "boolean",
+              default: true,
+              description:
+                "Use automatic Github PR template, e.g. .github/pull_request_template.md, disable with --no-template",
+            })
+
+            .option("mock-metadata", {
+              hidden: true,
+              type: "boolean",
+              default: false,
+              description: "Mock local store metadata for testing",
+            });
+        }
+      )
+
+      .command(
+        "fixup [commit]", // Positional command "fixup"
+        "Amend staged changes to a specific commit in history",
+        (yargs) => {
+          return yargs.positional("commit", {
+            type: "number",
+            default: 1,
+            description: [
+              "Relative number of commit to amend staged changes.",
+              "Most recent is 1, next is 2, etc.",
+            ].join("\n"),
+          });
+        }
+      )
 
       .option("verbose", {
         type: "boolean",
         alias: ["v"],
         default: false,
         description: "Print more detailed logs for debugging internals",
-      })
-
-      .option("update", {
-        type: "boolean",
-        alias: ["u", "upgrade"],
-        default: false,
-        description: "Check and install the latest version",
-      })
-
-      .option("branch", {
-        type: "string",
-        alias: ["b"],
-        description:
-          'Set the master branch name, defaults to "master" (or "main" if "master" is not found)',
-      })
-
-      .option("draft", {
-        type: "boolean",
-        alias: ["d"],
-        default: false,
-        description: "Open all PRs as drafts",
-      })
-
-      .option("write-state-json", {
-        hidden: true,
-        type: "boolean",
-        default: false,
-        description: "Write state to local json file for debugging",
-      })
-
-      .option("template", {
-        type: "boolean",
-        default: true,
-        description:
-          "Use automatic Github PR template, e.g. .github/pull_request_template.md, disable with --no-template",
-      })
-
-      .option("mock-metadata", {
-        hidden: true,
-        type: "boolean",
-        default: false,
-        description: "Mock local store metadata for testing",
       })
 
       // yargs default wraps to 80 columns
