9210aca756ee61772a1dc24ec8298ce5f7f6092d
magus <noah@iamnoah.com>
2024-09-27 23:29:32 -0700

    GitReviseTodo: note for stdio ignore

--- src/core/GitReviseTodo.ts
+++ src/core/GitReviseTodo.ts
@@ -136,6 +136,9 @@ GitReviseTodo.execute = async function grt_execute(args: ExecuteArgs) {
     `revise --edit -i ${args.rebase_merge_base}`,
   ];
 
+  // ignore here is important to prevent scrollback clear
+  // change to pipe to see output temporarily
+  // https://github.com/magus/git-stack-cli/commit/f9f10e3ac3cd9a35ee75d3e0851a48391967a23f
   await cli(command, { stdio: ["ignore", "ignore", "ignore"] });
 };
 
