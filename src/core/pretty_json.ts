1a942c0f6740877b56c0b9b238e3ca36d37f43cf
noah <noahj@openai.com>
2025-03-26 19:46:43 -0700

    pretty_json: Array<JSONValue>

--- src/core/pretty_json.ts
+++ src/core/pretty_json.ts
@@ -1,5 +1,11 @@
 export namespace pretty_json {
-  export type JSONValue = null | number | string | boolean | { [key: string]: JSONValue };
+  export type JSONValue =
+    | null
+    | number
+    | string
+    | boolean
+    | { [key: string]: JSONValue }
+    | Array<JSONValue>;
 }
 
 export function pretty_json<T extends pretty_json.JSONValue>(input: T): string {
