e8fca4c524277aa7a1e24beafd582b0b9417e8cc
noah <noahj@openai.com>
2025-05-02 02:26:59 -0700

    safe_quote: handle backslash and backtick

--- src/core/safe_quote.ts
+++ src/core/safe_quote.ts
@@ -1,10 +1,14 @@
 // escape double-quote for cli
 export function safe_quote(value: string) {
   let result = value;
+  result = result.replace(RE.all_backslash, "\\\\");
   result = result.replace(RE.all_double_quote, '\\"');
+  result = result.replace(RE.all_backtick, "\\`");
   return result;
 }
 
 const RE = {
   all_double_quote: /"/g,
+  all_backtick: /`/g,
+  all_backslash: /\\/g,
 };
