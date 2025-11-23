6c2fa9bfce57a548642d48a7a4c4d17ed38bcc24
magus <noah@iamnoah.com>
2025-11-22 23:51:38 -0800

    get_timeout_fn: wrap promise in timeout
    git-stack-id: noah/paint-test---4gwpqk3ea0ju14
    git-stack-title: AutoUpdate: skip timeout sleep if version successfully fetched

---
+++ src/core/get_timeout_fn.ts
@@ -0,0 +1,11 @@
+export function get_timeout_fn(ms: number, message: string) {
+  return function timeout<T>(promise: Promise<T>) {
+    let id: ReturnType<typeof setTimeout>;
+
+    const timeout = new Promise<never>((_resolve, reject) => {
+      id = setTimeout(() => reject(new Error(message)), ms);
+    });
+
+    return Promise.race([promise, timeout]).finally(() => clearTimeout(id));
+  };
+}
