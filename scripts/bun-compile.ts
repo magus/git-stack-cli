9976d48cf73e06af9e13d70dc8031fcc627b3060
magus <noah@iamnoah.com>
2025-11-23 16:47:46 -0800

    bun-compile: sourcemaps

--- scripts/bun-compile.ts
+++ scripts/bun-compile.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import * as util from "util";
 
 import * as file from "~/core/file";
+import { get_define } from "~/core/get_define";
 import { spawn } from "~/core/spawn";
 
 const parsed_args = util.parseArgs({
@@ -30,7 +31,7 @@ if (VERBOSE) {
 
 const REPO_ROOT = (await spawn.sync("git rev-parse --show-toplevel")).stdout;
 const DIST_DIR = path.join(REPO_ROOT, "dist");
-const INPUT_JS = path.join(DIST_DIR, "js", "index.js");
+const INPUT_JS = path.join(REPO_ROOT, "/src/index.tsx");
 
 if (!(await file.exists(INPUT_JS))) {
   console.error(`❌ Missing ${path.relative(REPO_ROOT, INPUT_JS)}`);
@@ -58,11 +59,23 @@ async function compile_target(args: CompileTargetArgs) {
 
   const start = Date.now();
 
+  // https://bun.com/docs/bundler/executables#build-time-constants
+  const defines: Array<string> = [];
+  for (const [key, value] of Object.entries(await get_define())) {
+    // --define BUILD_VERSION='"1.2.3"'
+    defines.push("--define", `${key}=${value}`);
+  }
+
   // pnpm bun build --compile --target=bun-darwin-arm64 ./dist/js/index.js --outfile git-stack-bun-darwin-arm64
+  // https://bun.com/docs/bundler/executables
   const bun_compile = await spawn.sync([
     "bun",
     "build",
     "--compile",
+    "--minify",
+    "--sourcemap",
+    // "--bytecode",
+    ...defines,
     `--target=${args.target}`,
     INPUT_JS,
     `--outfile=${outfile}`,
