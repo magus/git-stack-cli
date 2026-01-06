986444b83bae1ca0cdbecb35744d09a0516e9060
noah <noahj@openai.com>
2026-01-05 16:16:54 -0800

    Exit: write state to tmp file

--- src/app/Exit.tsx
+++ src/app/Exit.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 
+import fs from "node:fs/promises";
+import path from "node:path";
+
 import * as Ink from "ink-cjs";
 
 import { FormatText } from "~/app/FormatText";
 import { Store } from "~/app/Store";
 import { cli } from "~/core/cli";
 import { colors } from "~/core/colors";
+import { get_tmp_dir } from "~/core/get_tmp_dir";
+import * as json from "~/core/json";
+import { pretty_json } from "~/core/pretty_json";
 import { sleep } from "~/core/sleep";
 
 type Props = {
@@ -28,6 +34,14 @@ export function Exit(props: Props) {
 Exit.handle_exit = async function handle_exit(props: Props) {
   const state = Store.getState();
   const actions = state.actions;
+
+  // write state to file for debugging
+  if (state.select.debug(state)) {
+    const tmp_state_path = path.join(await get_tmp_dir(), "git-stack-state.json");
+    await fs.writeFile(tmp_state_path, pretty_json(json.serialize(state)));
+    const output = <Ink.Text color={colors.gray}>Wrote state to {tmp_state_path}</Ink.Text>;
+    actions.output(output);
+  }
 
   actions.debug(`[Exit] handle_exit ${JSON.stringify(props)}`);
 
